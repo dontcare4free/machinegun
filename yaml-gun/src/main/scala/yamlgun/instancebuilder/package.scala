9a79b3f6418b2e0db0ba4ddfcb721a01a6ab21bd
Nullable <teo@nullable.se>
2013-07-14 19:43:12 +0200

    Reorganized some stuff in InstanceBuilder

--- yaml-gun/src/main/scala/yamlgun/instancebuilder/package.scala
+++ yaml-gun/src/main/scala/yamlgun/instancebuilder/package.scala
@@ -4,11 +4,10 @@ import scala.reflect.runtime.universe._
 
 package object instancebuilder {
   private type RawCaseClassApi[T] = {
-    def copy(): CaseClass[T]
+    def copy(): T with CaseClassApi[T]
   }
   type CaseClassApi[T] = RawCaseClassApi[T] with Product
-  type CaseClass[T] = T with CaseClassApi[T]
 
-  implicit def caseClassInstanceBuilder[T <: CaseClassApi](default: T)(implicit t: TypeTag[T]) =
+  implicit def caseClassInstanceBuilder[T <: CaseClassApi[T]](default: T)(implicit t: TypeTag[T]) =
     new CaseClassInstanceBuilder[T](default)
 }
